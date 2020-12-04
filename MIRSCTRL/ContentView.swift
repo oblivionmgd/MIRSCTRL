3c3af0f4e8374e2028ddf1d9dd9067734b0924f2
OblivionMGD <ephixa6022@gmail.com>
2020-12-04 16:20:33 +0900

    add: homebackground

--- MIRSCTRL/ContentView.swift
+++ MIRSCTRL/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hi.")
-            .padding()
+        ZStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/){
+            HomeBackgound()
+            
+            Spacer()
+        }
     }
 }
 
