abd65fa8e127eb38ed991a594318433df579ff07
OblivionMGD <ephixa6022@gmail.com>
2020-12-04 16:19:59 +0900

    add: welcome text

--- MIRSCTRL/HomeBackgound.swift
+++ MIRSCTRL/HomeBackgound.swift
@@ -11,7 +11,13 @@ struct HomeBackgound: View {
     var body: some View {
         Image("MIRSHome")
             .resizable()
-            .frame(height: 920)
+            .frame(height: 872, alignment: .topLeading)
+            .overlay(Text("Welcome.")
+                        .fontWeight(.heavy)
+                        .font(.system(size: 40, weight: .heavy))
+                        .foregroundColor(Color(red: 250, green: 247, blue: 237, opacity: 1.0))
+                        .multilineTextAlignment(.leading)
+                        .offset(x: -70,y: -350))
     }
 }
 
