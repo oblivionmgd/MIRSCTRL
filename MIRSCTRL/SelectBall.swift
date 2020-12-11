c33f0d8658684d5d882ff53c433042387939ec6c
OblivionMGD <ephixa6022@gmail.com>
2020-12-11 14:24:46 +0900

    add: ball select page

---
+++ MIRSCTRL/SelectBall.swift
@@ -0,0 +1,20 @@
+//
+//  SelectBall.swift
+//  MIRSCTRL
+//
+//  Created by 中村伸哉 on 2020/12/11.
+//
+
+import SwiftUI
+
+struct SelectBall: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectBall_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectBall()
+    }
+}
