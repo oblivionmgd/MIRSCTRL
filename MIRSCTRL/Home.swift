2642c39c153257042f05c3ba452a4a97806837d3
OblivionMGD <ephixa6022@gmail.com>
2020-12-12 00:08:18 +0900

    upgrade: ball select transition

--- MIRSCTRL/Home.swift
+++ MIRSCTRL/Home.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 
 struct Home: View {
-    @State var isPresented = false
+    @State private var startCollect = false
+    
+    let darkRed = Color(red: 199 / 255, green: 91 / 255, blue: 87 / 255)
+    let backgroundColor = Color(red: 237 / 255, green: 221 / 255, blue: 199 / 255)
+    
     var body: some View {
-        let darkRed = Color(red: 199 / 255, green: 91 / 255, blue: 87 / 255)
-        let backgroundColor = Color(red: 237 / 255, green: 221 / 255, blue: 199 / 255)
-        
         ZStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/){
             HomeBackgound()
             VStack {
                 Spacer().frame(height: 500)
-                Button(action: {}) {
+                Button(action:{ startCollect.toggle() }) {
                     VStack {
                         Text("あつめる")
                             .font(Font.custom("HiraginoSans-W6", size: 18))
@@ -31,6 +32,9 @@ struct Home: View {
                 }
                 .softButtonStyle(Circle(), padding: 60, mainColor: backgroundColor, darkShadowColor: Color(red: 204 / 255, green: 187 / 255, blue: 170 / 255), lightShadowColor: Color(red: 1, green: 1, blue: 1, opacity: 0.4), pressedEffect: .hard)
                 .frame(width: 250, height: 250)
+            }
+            .sheet(isPresented: $startCollect) {
+                SelectBall()
             }
         }
     }
