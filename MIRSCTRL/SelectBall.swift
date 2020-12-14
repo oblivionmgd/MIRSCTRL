7697e381ea8eb6c2d4094027f1387af440989cc8
OblivionMGD <ephixa6022@gmail.com>
2020-12-14 16:40:23 +0900

    upgrade: animation

--- MIRSCTRL/SelectBall.swift
+++ MIRSCTRL/SelectBall.swift
@@ -10,7 +10,7 @@ import SwiftUI
 extension Animation {
     static func ripple() -> Animation {
         Animation.spring(dampingFraction: 0.3)
-            .speed(1)
+            .speed(1.5)
     }
 }
 
@@ -21,7 +21,7 @@ let black = Color(red: 40 / 255, green: 54 / 255, blue: 74 / 255)
 
 struct SelectBall: View {
     @State var isProcessing = false
-    @State var circleSize: CGFloat = 700
+    @State var objectOpacity:Double  = 1
 
     var body: some View {
         ZStack {
@@ -29,7 +29,7 @@ struct SelectBall: View {
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             Circle()
                 .fill(green)
-                .frame(width: circleSize, height: circleSize, alignment: .bottom)
+                .frame(width: 700, height: 700, alignment: .bottom)
                 .offset(y: 300)
                 .scaleEffect(isProcessing ? 8 : 1)
                 .animation(.ripple())
@@ -38,22 +38,31 @@ struct SelectBall: View {
                 Text("Select Ball.")
                     .font(.system(size: 48, weight: .heavy))
                     .foregroundColor(black)
+                    .opacity(objectOpacity)
                 Text("Swipe to change ball")
                     .font(.system(size: 24, weight: .medium))
                     .foregroundColor(black)
+                    .opacity(objectOpacity)
                 BallTabView()
                     .frame(width:UIScreen.main.bounds.width)
+                    .scaleEffect(isProcessing ? 0 : 1)
+                    .opacity(objectOpacity)
                 Button(action:{
                     isProcessing.toggle()
+                    withAnimation(.easeOut(duration: 0.5)){
+                        self.objectOpacity = 0
+                    }
                 }){
                     Text("    START    ")
                         .font(.system(size: 36, weight: .bold))
                         .foregroundColor(backgroundColor)
                 }
                 .softButtonStyle(RoundedRectangle(cornerRadius: 36), mainColor: green, darkShadowColor: Color(red: 48 / 255, green: 78 / 255, blue: 78 / 255), lightShadowColor: Color(red: 64 / 255, green: 106 / 255, blue: 106 / 255), pressedEffect: .hard)
+                .opacity(objectOpacity)
             }
         }
         .offset(y: -20)
+        
     }
 }
 
