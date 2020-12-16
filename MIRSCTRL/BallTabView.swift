0cb1caf70cacf4ba7e6f0419babc89ecde642829
OblivionMGD <ephixa6022@gmail.com>
2020-12-16 15:30:07 +0900

    upgrade: make ball identifiable protocol

--- MIRSCTRL/BallTabView.swift
+++ MIRSCTRL/BallTabView.swift
@@ -7,51 +7,47 @@
 
 import SwiftUI
 
+struct Ball: Identifiable {
+    var id: Int
+    var ballImageName: String
+    var ballName: String
+    var ballDescription: String
+}
+
 struct BallTabView: View {
-    var body: some View {
-        PageView()
+    
+    let balls: [Ball] = [
+        Ball(id: 0, ballImageName: "tennisballimg", ballName: "Tennis Ball", ballDescription: "Storage limit: \n About 8 ~ 10 balls"),
+        Ball(id: 1, ballImageName: "pingpongimg", ballName: "Table Tennis Ball", ballDescription: "Storage limit: \n About 50 ~ 70 balls")
+    ]
+    
+    
+    var body: some View{
+        TabView{
+            ForEach(balls) { ball in
+                VStack {
+                    Image(ball.ballImageName)
+                        .resizable()
+                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width)
+                    Text(ball.ballName)
+                        .font(.system(size: 36, weight: .bold))
+                        .foregroundColor(Color(red: 237 / 255, green: 221 / 255, blue: 199 / 255))
+
+                    Text(ball.ballDescription)
+                        .font(.system(size: 12, weight: .bold))
+                        .foregroundColor(Color(red: 237 / 255, green: 221 / 255, blue: 199 / 255))
+                        .multilineTextAlignment(.leading)
+                }
+                .offset(y: -20)
+            }
+        }
+//        .aspectRatio(contentMode: .fit)
+        .tabViewStyle(PageTabViewStyle())
     }
 }
 
 struct BallTabView_Previews: PreviewProvider {
     static var previews: some View {
         BallTabView()
-    }
-}
-
-struct PageView: View {
-    var body: some View{
-        TabView{
-            VStack {
-                Image("tennisballimg")
-                    .resizable()
-                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width)
-                Text("Tennis ball")
-                    .font(.system(size: 36, weight: .bold))
-                    .foregroundColor(Color(red: 237 / 255, green: 221 / 255, blue: 199 / 255))
-
-                Text("Storage limit: \n About 8 ~ 10 balls")
-                    .font(.system(size: 12, weight: .bold))
-                    .foregroundColor(Color(red: 237 / 255, green: 221 / 255, blue: 199 / 255))
-                    .multilineTextAlignment(.leading)
-            }
-            .offset(y: -20)
-            VStack {
-                Image("pingpongimg")
-                    .resizable()
-                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width)
-                Text("Table tennis ball")
-                    .font(.system(size: 36, weight: .bold))
-                    .foregroundColor(Color(red: 237 / 255, green: 221 / 255, blue: 199 / 255))
-
-                Text("Storage limit: \n About 50 ~ 70 balls")
-                    .font(.system(size: 12, weight: .bold))
-                    .foregroundColor(Color(red: 237 / 255, green: 221 / 255, blue: 199 / 255))
-                    .multilineTextAlignment(.leading)
-            }
-            .offset(y: -20)
-        }
-//        .aspectRatio(contentMode: .fit)
-        .tabViewStyle(PageTabViewStyle())
     }
 }
